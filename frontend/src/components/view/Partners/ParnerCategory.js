0b6d8944097de7833c932623bd5ef8a54d1da9ee
Deshan Rajapaksha <lankajayamini1133@gmail.com>
2025-02-07 16:41:27 +0530

    changes

--- frontend/src/components/view/Partners/ParnerCategory.js
+++ frontend/src/components/view/Partners/ParnerCategory.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 // import axios from 'axios';
 
 import Header from '../Headers/Header';
-import image3 from '../../image/image3.png';
+// import international from '../../image/international.png';
+// import local from '../../image/local.png';
 
 
 const PartnerCategory = () => {
@@ -19,9 +20,8 @@ const PartnerCategory = () => {
                 <h3 class="text-uppercase" style= {{ margin : "50px 0px 60px 0px", fontWeight:"bold", textAlign:"center" , fontWeight : "800"}}>Partners</h3>
                     <div class="col">
                         <div class="card" style={{ borderRadius: "35px", margin:"40px", boxShadow: "2px 2px 10px rgba(0,0,0,0.3)"}}>
+                            {/* <img src={international} class="card-img-top" alt="..."/> */}
                             <div class="card-body">
-                                <h5 class="card-title" style = {{ fontWeight : "700", margin: "20px 0px 0px 0px" }}>International Partners</h5>
-                                <p class="card-text" style = {{ margin : "30px 20px 0px 20px" }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="/international" style = {{ margin : "60px 0px 0px 0px" }} class="btn btn-primary">International</a>
                             </div>
                         </div>
@@ -29,9 +29,8 @@ const PartnerCategory = () => {
 
                     <div class="col">
                         <div class="card" style={{ borderRadius: "35px",margin:"40px", boxShadow: "2px 2px 10px rgba(0,0,0,0.3)" }}>
+                            {/* <img src={international} class="card-img-top" alt="..."/> */}
                             <div class="card-body">
-                                <h5 class="card-title" style = {{ fontWeight : "700" , margin: "20px 0px 0px 0px"}}>Local Partners</h5>
-                                <p class="card-text" style = {{ margin : "30px 20px 0px 20px" }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="/local" style = {{ margin : "60px 0px 0px 0px" }} class="btn btn-primary">Local</a>
                             </div>
                         </div>
