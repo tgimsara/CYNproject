ef6097fc36d62a94695f2320e53db601283c681d
Deshan Rajapaksha <lankajayamini1133@gmail.com>
2025-02-07 13:08:18 +0530

    partners

--- frontend/src/components/view/Partners/International.js
+++ frontend/src/components/view/Partners/International.js
@@ -1,14 +1,311 @@
 import React from "react";
 
-import NotRegHeader from "../Headers/NotRegHeader";
+import Header from "../Headers/Header";
+
+import dreams from '../../image/dreams.png';
+import beauty from '../../image/beauty.png';
+import citizen from '../../image/citizen.png';
+import cricket from '../../image/cricket.png';
+import education from '../../image/education.png';
+import edutourism from '../../image/edutourism.png';
+import foundation from '../../image/foundation.png';
+import lpec from '../../image/lpec.png';
+import slppca from '../../image/slppca.png';
+import tour from '../../image/tour.png';
+import youth from '../../image/youth.png';
+import youthcrop from '../../image/youthcrop.png';
+import youthsport from '../../image/youthsport.png';
 
 const International = () => {
 
+    return(
+
     <div>
 
-        <NotRegHeader />
-    </div>
+        <Header />
 
+         <div class="container text-center">
+                    <div class="row">
+                    <h3 class="text-uppercase" style= {{ margin : "50px 0px 60px 0px", fontWeight:"bold", textAlign:"center" , fontWeight : "800"}}>International Partners</h3>
+        
+                        <div class="col">
+                            <div class="card" style={{ margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={dreams} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Audacious Dreams Foundation</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={slppca} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Malaysian Indian Youth Council </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={foundation} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Commonwealth Youth Council</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={beauty} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Commonwealth Students' Association</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                    </div>
+        
+                    <div class="row">
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={youth} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>The Commonwealth</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={youthcrop} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Royal Commonwealth Society</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={youthsport} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Commonwealth Youth Exchange Council </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={education} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Commonwealth Youth Peace Ambassadors Network - CYPAN</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+        
+                    <div class="row">
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={dreams} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>National Youth Achievement Award Council</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={cricket} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Glocal Pvt. Ltd.</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={citizen} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Malaysia International Hair & Beauty Association</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={edutourism} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Management and Science University (MSU), Malaysia</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+        
+                    <div class="row">
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Malaysian Esthetic Association</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>TMC Aesthetic College, Malaysia</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={lpec} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}> University of Cyberjaya, Malaysia</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>HELP University</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>University of Putra Malaysia (UPM) </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>International Counselling Association, Malaysia - PERKAMA</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={lpec} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Malaysian Mental Health Association (MMHA)</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Thrive WELL</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}> Kementerian Kesihatan (NCEMH)</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Sri Lanka High Commission in Malaysia </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={lpec} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}> Sunway University Malaysia </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>University College Sedaya International - UCSI University</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Asia Pacific University</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Malaysian Society of Clinical Psychology</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                            <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={lpec} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Kuala Lumpur Hospital - Counselling Unit </p>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col">
+                        <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                                <img src={tour} class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Reskills Skills Development Platform</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+        
+                </div>
+
+    </div>
+)
 } 
 
 export default International
---
+++ frontend/src/components/view/Partners/Local.js
@@ -0,0 +1,187 @@
+import React from "react";
+
+import Header from "../Headers/Header";
+import '../Partners/partner.css';
+
+import british from '../../image/british.png';
+import beauty from '../../image/beauty.png';
+import citizen from '../../image/citizen.png';
+import cricket from '../../image/cricket.png';
+import education from '../../image/education.png';
+import edutourism from '../../image/edutourism.png';
+import foundation from '../../image/foundation.png';
+import lpec from '../../image/lpec.png';
+import slppca from '../../image/slppca.png';
+import tour from '../../image/tour.png';
+import youth from '../../image/youth.png';
+import youthcrop from '../../image/youthcrop.png';
+import youthsport from '../../image/youthsport.png';
+
+const Local = () => {
+
+
+    return (
+        <div>
+             <Header/>
+           
+
+        <div class="container text-center">
+            <div class="row">
+            <h3 class="text-uppercase" style= {{ margin : "50px 0px 60px 0px", fontWeight:"bold", textAlign:"center" , fontWeight : "800"}}>Local Partners</h3>
+
+                <div class="col">
+                    <div class="card" style={{ margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={lpec} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>LPEC Campus Pvt Ltd</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={slppca} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 0px 0px", textAlign:"left"}}>Sri Lanka Professioanal Psychological Counsellor’s Association</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={foundation} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Sri Lanka Foundation Institute</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={beauty} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 24px 0px", textAlign:"left"}}>Ceylon Beauty and Aeatheticians Association</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={youth} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>National Youth Serrvices Council</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={youthcrop} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>National Youth Corps </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={youthsport} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Ministry of Youth and Sports</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={education} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Education Ministry</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={british} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>British Council Sri Lanka</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={cricket} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 24px 0px", textAlign:"left"}}>Sri Lanka Cricket Association for Physically Chalenged</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={citizen} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Global Citizens</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={edutourism} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Ceylon Edutourism Pvt Ltd</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="card" style={{ margin:"20px" }}>
+                        
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={tour} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 48px 0px", textAlign:"left"}}>Global Study Tours Pvt Ltd</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{margin:"20px", boxShadow: "2px 2px 10px rgba(0,0,0,0.1)" }}>
+                        <img src={lpec} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text" style = {{ fontWeight:"bold", margin: "20px 0px 24px 0px", textAlign:"left"}}>International Learning Pathways Pvt Ltd</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style={{ margin:"20px" }}>
+                        
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
+)
+} 
+
+export default Local
